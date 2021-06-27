4d9c8e7e0fe3d1f22a1db81eb0bca11e752f7146
Thaz <76603398+thasinaz@users.noreply.github.com>
2021-06-27 09:56:21 +0800

    Add Representing Code challenge 3

---
+++ jlox/com/craftinginterpreters/lox/AstRpnConverter.java
@@ -0,0 +1,53 @@
+package com.craftinginterpreters.lox;
+
+class AstRpnConverter implements Expr.Visitor<String> {
+  String convert(Expr expr) {
+    return expr.accept(this);
+  }
+
+  @Override
+  public String visitBinaryExpr(Expr.Binary expr) {
+    return rpn(expr.operator.lexeme,
+                        expr.left, expr.right);
+  }
+
+  @Override
+  public String visitGroupingExpr(Expr.Grouping expr) {
+    return rpn("group", expr.expression);
+  }
+
+  @Override
+  public String visitLiteralExpr(Expr.Literal expr) {
+    if (expr.value == null) return "nil";
+    return expr.value.toString();
+  }
+
+  @Override
+  public String visitUnaryExpr(Expr.Unary expr) {
+    return rpn(expr.operator.lexeme, expr.right);
+  }
+
+  private String rpn(String name, Expr... exprs) {
+    StringBuilder builder = new StringBuilder();
+
+    for (Expr expr : exprs) {
+      builder.append(expr.accept(this));
+      builder.append(" ");
+    }
+    builder.append(name);
+
+    return builder.toString();
+  }
+
+  public static void main(String[] args) {
+    Expr expression = new Expr.Binary(
+        new Expr.Unary(
+            new Token(TokenType.MINUS, "-", null, 1),
+            new Expr.Literal(123)),
+        new Token(TokenType.STAR, "*", null, 1),
+        new Expr.Grouping(
+            new Expr.Literal(45.67)));
+
+    System.out.println(new AstRpnConverter().convert(expression));
+  }
+}
