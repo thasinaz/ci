411889e47b2484435516240f19c8154f2a4fb0b8
Thaz <76603398+thasinaz@users.noreply.github.com>
2024-03-27 15:03:24 +0800

    Support OP_CONSTANT_LONG for compiler

--- clox/compiler.c
+++ clox/compiler.c
@@ -129,18 +129,8 @@ static void emitReturn() {
   emitByte(OP_RETURN);
 }
 
-static uint8_t makeConstant(Value value) {
-  int constant = addConstant(currentChunk(), value);
-  if (constant > UINT8_MAX) {
-    error("Too many constants in one chunk.");
-    return 0;
-  }
-
-  return (uint8_t)constant;
-}
-
 static void emitConstant(Value value) {
-  emitBytes(OP_CONSTANT, makeConstant(value));
+  writeConstant(currentChunk(), value, parser.previous.line);
 }
 
 static void initCompiler(Compiler* compiler) {
